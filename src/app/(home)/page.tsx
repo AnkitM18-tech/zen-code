d5f8ae69b32c086f7ce466c9a52575f8086aa5f1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-12-21 16:51:27 +0530

    EditorPanel Component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <div className="min-h-screen">
       <div className="max-w-[1800px] mx-auto p-4">
         <Header />
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <EditorPanel />
-        <OutputPanel />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <EditorPanel />
+          <OutputPanel />
+        </div>
       </div>
     </div>
   );
